c56598942a8ec381d0c6ffe4afc21f9a085b79b3
Tyler Miller <tyler.e.miller1995@gmail.com>
2024-02-04 15:31:25 -0600

    Added gRPC client and server functionality
    - Introduced a new gRPC client with unary and streaming capabilities.
    - Created a gRPC server that supports unary, client-streaming, server-streaming, and bidirectional streaming methods.
    - Added .proto files for defining the service contracts.
    - Included necessary package references for protobuf and Grpc in project files.
    - Updated .gitignore to ignore JetBrains Rider IDE specific files.

---
+++ FirstGRPC.Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using Basics;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+var options = new GrpcChannelOptions
+{
+    Credentials = ChannelCredentials.Insecure
+};
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5242", options);
+var client = new FirstServiceDefinition.FirstServiceDefinitionClient(channel);
+
+// Unary(client);
+await ClientStreaming(client);
+return;
+
+static void Unary(FirstServiceDefinition.FirstServiceDefinitionClient client)
+{
+    var request = new Request { Content = "Hello" };
+    var response = client.Unary(request);
+}
+
+static async Task ClientStreaming(FirstServiceDefinition.FirstServiceDefinitionClient client)
+{
+    using var call = client.ClientStream();
+    for (var i = 0; i < 1_000; i++)
+    {
+        await call.RequestStream.WriteAsync(new Request { Content = i.ToString() });
+    }
+
+    await call.RequestStream.CompleteAsync();
+    var response = await call;
+    Console.WriteLine(response.Message);
+}
---
+++ FirstGRPC/Services/FirstService.cs
@@ -0,0 +1,49 @@
+﻿using Basics;
+using Grpc.Core;
+
+namespace FirstGRPC.Services;
+
+public class FirstService : FirstServiceDefinition.FirstServiceDefinitionBase
+{
+    public override Task<Response> Unary(Request request, ServerCallContext context)
+    {
+        var response = new Response { Message = $"{request.Content} from server" };
+        return Task.FromResult(response);
+    }
+
+    public override async Task<Response> ClientStream(IAsyncStreamReader<Request> requestStream,
+        ServerCallContext context)
+    {
+        var response = new Response { Message = "Received." };
+        while (await requestStream.MoveNext())
+        {
+            var requestPayload = requestStream.Current;
+            Console.WriteLine($"Received: {requestPayload.Content}");
+            response.Message = requestPayload.ToString();
+        }
+
+        return response;
+    }
+
+    public override async Task ServerStream(Request request, IServerStreamWriter<Response> responseStream,
+        ServerCallContext context)
+    {
+        for (var i = 0; i < 100; i++)
+        {
+            var response = new Response { Message = i.ToString() };
+            await responseStream.WriteAsync(response);
+        }
+    }
+
+    public override async Task BiDirectionalStream(IAsyncStreamReader<Request> requestStream, IServerStreamWriter<Response> responseStream,
+        ServerCallContext context)
+    {
+        var response = new Response { Message = string.Empty };
+        while (await requestStream.MoveNext())
+        {
+            var requestPayload = requestStream.Current;
+            response.Message = requestPayload.ToString();
+            await responseStream.WriteAsync(response);
+        }
+    }
+}
